b88d4107105fb29d6db9d10ec1d034186c9ca3c9
Hein-Pieter van Braam <hp@tmm.cx>
2014-10-08 00:44:04 +0200

    Add GPLv2 or later license

--- app/dns.rb
+++ app/dns.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/env ruby
 
+# Copyright (C) 2014 Hein-Pieter can Braam <hp@tmm.cx>
+#
+# This file is part of Simple RESTful DNS api.
+#
+# Simple RESTful DNS api is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Simple RESTful DNS api is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Simple RESTful DNS api. If not, see <http://www.gnu.org/licenses/>.
+
 require 'rubygems'
 require 'sinatra'
 require 'json'
